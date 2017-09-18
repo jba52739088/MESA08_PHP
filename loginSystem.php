3ca57160c1258584cea1a0a436db490d8d9c1059
HuangEnYu <jba52739088@gmail.com>
2017-09-18 19:26:57 +0800

    v1

---
+++ loginSystem.php
@@ -0,0 +1,50 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="widht=device.width, initial.scale=1" />
+    <meta name="robots" content="follow">
+
+    <tittle>Page Tittle</tittle>
+
+    <base hret="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css" />
+
+</head>
+
+<body>
+
+<div class="uk-section uk-container">
+    <div class=".uk-guid.uk-child-width-1-3@s.uk-child-width-1-1[uk-grid]" uk-grid="">
+                <form class="uk-form-horizontal uk-margin-large">
+
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-horizontal-text">Email</label>
+                        <div class="uk-form-controls">
+
+                            <input class="uk-input" id="form-horizontal-text" type="email" placeholder="Email....">
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-horizontal-text">Password</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input" id="form-horizontal-text" type="password" placeholder="password....">
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        <button class="uk-button.uk-button-default" type='submit'>Login</button>
+
+                    </div>
+
+                </form>
+    </div>
+</div>
+
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script>
+</body>
+</html>
