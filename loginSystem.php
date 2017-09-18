9c73cdfca684d3598e95b273bc326a357c16cce4
HuangEnYu <jba52739088@gmail.com>
2017-09-18 19:59:14 +0800

    v2

--- loginSystem.php
+++ loginSystem.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="widht=device.width, initial.scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="follow">
 
     <tittle>Page Tittle</tittle>
@@ -15,20 +15,20 @@
 <body>
 
 <div class="uk-section uk-container">
-    <div class=".uk-guid.uk-child-width-1-3@s.uk-child-width-1-1[uk-grid]" uk-grid="">
-                <form class="uk-form-horizontal uk-margin-large">
+    <div class="uk-guid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+                <form class="uk-form-stacked js-login">
 
                     <div class="uk-margin">
                         <label class="uk-form-label" for="form-horizontal-text">Email</label>
                         <div class="uk-form-controls">
 
-                            <input class="uk-input" id="form-horizontal-text" type="email" placeholder="Email....">
+                            <input class="uk-input" id="form-horizontal-text" type="email" required="required" placeholder="Email....">
                         </div>
                     </div>
                     <div class="uk-margin">
                         <label class="uk-form-label" for="form-horizontal-text">Password</label>
                         <div class="uk-form-controls">
-                            <input class="uk-input" id="form-horizontal-text" type="password" placeholder="password....">
+                            <input class="uk-input" id="form-horizontal-text" type="password" required="required" placeholder="password....">
                         </div>
                     </div>
                     <div class="uk-margin">
